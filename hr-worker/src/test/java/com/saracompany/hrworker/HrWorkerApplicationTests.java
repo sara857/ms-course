652da146323fd962fde7d54cd4ccdf20b39f647c
Sara Moreira de Carvalho <sara.mcarvalho@hotmail.com>
2023-07-28 14:43:29 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/saracompany/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saracompany.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
